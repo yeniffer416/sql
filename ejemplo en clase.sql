f801479b9b9eed80e2e42b25dfe814f383e2e2f1
yeniffer416 <yaespinosa416@misena.edu.co>
2022-04-01 10:58:01 -0500

    actualizacion 1

--- ejemplo en clase.sql
+++ ejemplo en clase.sql
@@ -7,3 +7,13 @@ descripcion text(40) not null,
 precio float not null,
 constraint pk_venta primary key (id)
 );
+
+
+mysql> create database restaurante;
+mysql> use restaurante;
+mysql> describe usuario;
+mysql> insert into usuario (id_usuario,nom_usuario,ape_usuario,fechares_usuario)values
+    -> (1075241614, "laura", "castellanos", 20100312);
+    mysql> insert into usuario (id_usuario,nom_usuario,ape_usuario,fechares_usuario)values
+    -> (1075273624, "mya", "rodriguez", 20110517);
+    mysql> select * from usuario;
